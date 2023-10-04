2255a500ceebfba0a754b9864610d7fddf0505f9
Merijn <merijn@ponzo.nl>
2023-10-04 13:15:17 +0200

    biconupdate

---
+++ jsx/BiconColumns.jsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+const BiconColumns = (props) => (
+  <svg
+    width={props.width || "1em"}
+    height={props.height || "1em"}
+    viewBox="0 0 28 28"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    role="img"
+    {...props}
+  >
+    <rect x={4} y={4} width={8} height={20} fill="black" />
+    <rect x={17} y={4} width={8} height={20} fill="black" />
+  </svg>
+);
+
+export default BiconColumns;
---
+++ jsx/BiconPresetflex1col.jsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+const BiconPresetflex1col = (props) => (
+  <svg
+    width={props.width || "1em"}
+    height={props.height || "1em"}
+    viewBox="0 0 60 40"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    role="img"
+    {...props}
+  >
+    <rect width={60} height={40} fill="white" />
+    <rect x={7} y={7} width={44} height={27} fill="black" />
+  </svg>
+);
+
+export default BiconPresetflex1col;
