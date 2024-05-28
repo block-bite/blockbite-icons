e5ef7ec171b2604d75ea691d4a3f8426e40d25ba
Merijn <merijn@ponzo.nl>
2024-05-28 16:20:17 +0200

    test fix

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
-const fs = require('fs');
-const path = require('path');
+const path = require("path");
 
-// Replace with the actual path to your icons directory
-const iconsDir = path.join(__dirname, 'jsx');
+let fs;
+if (typeof window === "undefined") {
+  // We are in a Node.js environment
+  fs = require("fs");
 
-fs.readdir(iconsDir, (err, files) => {
-  if (err) {
-    console.error('Could not list the directory.', err);
-    process.exit(1);
-  }
+  // Replace with the actual path to your icons directory
+  const iconsDir = path.join(__dirname, "jsx");
 
-  const exports = files.map(file => {
-    // Remove the file extension to get the component name
-    const componentName = path.basename(file, path.extname(file));
-    return `export function ${componentName}(): JSX.Element;`;
-  });
-
-  const content = `declare module 'blockbite-icons' {\n  ${exports.join('\n  ')}\n}`;
-
-  fs.writeFile(path.join(__dirname, 'types', 'index.d.ts'), content, err => {
+  fs.readdir(iconsDir, (err, files) => {
     if (err) {
-      console.error('Error writing index.d.ts', err);
-    } else {
-      console.log('index.d.ts has been generated');
+      console.error("Could not list the directory.", err);
+      process.exit(1);
     }
+
+    const exports = files.map((file) => {
+      // Remove the file extension to get the component name
+      const componentName = path.basename(file, path.extname(file));
+      return `export function ${componentName}(): JSX.Element;`;
+    });
+
+    const content = `declare module 'blockbite-icons' {\n  ${exports.join(
+      "\n  "
+    )}\n}`;
+
+    fs.writeFile(
+      path.join(__dirname, "types", "index.d.ts"),
+      content,
+      (err) => {
+        if (err) {
+          console.error("Error writing index.d.ts", err);
+        } else {
+          console.log("index.d.ts has been generated");
+        }
+      }
+    );
   });
-});
+}
