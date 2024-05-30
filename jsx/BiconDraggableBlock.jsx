756b2ff937b9d3949dccaaf942dcac273cfe97c9
Merijn <merijn@ponzo.nl>
2024-05-30 14:10:55 +0200

    testtest

--- jsx/BiconDraggableBlock.jsx
+++ jsx/BiconDraggableBlock.jsx
@@ -10,21 +10,21 @@ const BiconDraggableBlock = (props) => (
     role="img"
     {...props}
   >
-    <rect width="50" height="50" fill="black" />
+    <rect width={50} height={50} fill="black" />
     <path
-      fill-rule="evenodd"
-      clip-rule="evenodd"
+      fillRule="evenodd"
+      clipRule="evenodd"
       d="M9 19H12V21.75H11.75V22H9V19ZM9 38V41H12V38.25H11.75V38H9ZM28 41H31V38H28.25V38.25H28V41ZM31 22V19H28V21.75H28.25V22H31ZM17.25 19V22H22.75V19H17.25ZM31 27.25H28V32.75H31V27.25ZM22.75 41V38H17.25V41H22.75ZM9 32.75H12V27.25H9V32.75Z"
       fill="#F5F5F5"
     />
-    <rect x="18" y="10" width="22" height="22" fill="#F5F5F5" />
+    <rect x={18} y={10} width={22} height={22} fill="#F5F5F5" />
     <line
-      x1="32.1232"
-      y1="17.9357"
-      x2="25.2482"
-      y2="24.8107"
+      x1={32.1232}
+      y1={17.9357}
+      x2={25.2482}
+      y2={24.8107}
       stroke="black"
-      stroke-width="3"
+      strokeWidth={3}
     />
     <path
       d="M36.2188 13.7813L35.875 21.6875L28.3125 14.125L36.2188 13.7813Z"
